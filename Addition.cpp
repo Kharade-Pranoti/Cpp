7803ee6fbd4e9d849afd0097fee0b1b8e5f2ad8a
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2024-04-11 07:49:56 +0530

    Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,17 @@
+// Write a program for addition of two numbers
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+  int number1 , number2, addition;
+
+  cout<<"Number1 = 34 Number2 = 56"<<endl;
+  
+  addition = number1 + number2;
+  cout<<"Addition of two numbers : "<<addition;
+  
+  return 0;
+  
+}
